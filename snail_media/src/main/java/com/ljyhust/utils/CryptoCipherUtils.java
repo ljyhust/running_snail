97eb26f0c8e1bb7d244145848ba833661e189e2d
ljyhust <ljyhust@163.com>
2018-05-06 18:44:09 +0800

    完善音频流加密

--- snail_media/src/main/java/com/ljyhust/utils/CryptoCipherUtils.java
+++ snail_media/src/main/java/com/ljyhust/utils/CryptoCipherUtils.java
@@ -19,6 +19,12 @@ public class CryptoCipherUtils {
     private static final int DECRYPT_MODE = Cipher.DECRYPT_MODE;
     private static final int BLOCK_SIZE = 1024;
 
+    /**
+     * 密钥生成方法
+     * @param Algorithm 加密算法类型  AES/RSA等
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
     public static String generateKey(final String Algorithm) throws NoSuchAlgorithmException {
         KeyGenerator keyGenerator = KeyGenerator.getInstance(Algorithm);
         SecureRandom random = new SecureRandom();
@@ -34,20 +40,21 @@ public class CryptoCipherUtils {
             Cipher cipher = Cipher.getInstance("AES");
             cipher.init(MODE, key);
             CipherOutputStream out = new CipherOutputStream(os, cipher);
-            int c = 0;
-            while ((c = is.read()) != -1) {
-                out.write(c);
-            }
-//            byte[] buff = new byte[BLOCK_SIZE];
-//            int length;
-//            while ((length = is.read(buff)) != -1) {
-//                if (length == BLOCK_SIZE)
-//                    out.write(buff, 0, length);
-//                else {
-//                    //byte[] finalBytes = cipher.doFinal(buff, 0, length);
-//                    os.write(buff);
-//                }
+//            int c = 0;
+//            while ((c = is.read()) != -1) {
+//                out.write(c);
 //            }
+            byte[] buff = new byte[BLOCK_SIZE];
+            int length;
+            while ((length = is.read(buff, 0, buff.length)) != -1) {
+                out.write(buff, 0, length);
+                /*if (length == BLOCK_SIZE)
+                    out.write(buff, 0, length);
+                else {
+                    byte[] finalBytes = cipher.doFinal(buff, 0, length);
+                    os.write(buff, 0, length);
+                }*/
+            }
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (NoSuchPaddingException e) {
