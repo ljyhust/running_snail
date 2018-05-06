33f4ebac9a8dcd8ee5b086defa5b4359976a4e13
ljyhust <ljyhust@163.com>
2018-05-06 11:01:17 +0800

    application.yml配置文件

---
+++ snail_media/src/test/java/com/ljyhust/SnailMediaApplicationTests.java
@@ -0,0 +1,28 @@
+package com.ljyhust;
+
+import com.ljyhust.utils.AESUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnailMediaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
