9e648c65c4147205df5a1514e84a9043968cc59e
ljyhust <ljyhust@163.com>
2018-05-30 23:16:45 +0800

    【tree操作】

---
+++ snail_algorithm/src/main/java/com/ljyhust/algorithm/TreeNode.java
@@ -0,0 +1,15 @@
+package com.ljyhust.algorithm;
+
+/**
+ * Created by Administrator on 2018/5/30.
+ */
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
