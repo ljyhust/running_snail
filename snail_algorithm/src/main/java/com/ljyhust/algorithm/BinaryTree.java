80871cf5c9e522faa06b7fe4884b153c7cfba090
ljyhust <ljyhust@163.com>
2018-06-07 19:45:54 +0800

    对称二叉树判断

--- snail_algorithm/src/main/java/com/ljyhust/algorithm/BinaryTree.java
+++ snail_algorithm/src/main/java/com/ljyhust/algorithm/BinaryTree.java
@@ -1,6 +1,9 @@
 package com.ljyhust.algorithm;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -37,5 +40,129 @@ public class BinaryTree {
         }
 
         return res;
+    }
+
+    /**
+     * 中序遍历
+     * @param root
+     * @return
+     */
+    public void printTreeByMidium(TreeNode root, List<String> res) {
+        if (root == null) {
+            return;
+        }
+        if (null == root.left && null != root.right) {
+            res.add("#");
+            res.add(root.val + "");
+            printTreeByMidium(root.right, res);
+        } else if (null != root.left && null == root.right) {
+            printTreeByMidium(root.left, res);
+            res.add(root.val + "");
+            res.add("#");
+        } else {
+            printTreeByMidium(root.left, res);
+            res.add(root.val + "");
+            printTreeByMidium(root.right, res);
+        }
+    }
+
+    /**
+     * 二叉树序列化
+     * @param root
+     * @return
+     */
+    public String Serialize(TreeNode root) {
+        // 用先序遍历
+        String res = "";
+        if (null == root) {
+            res = res + "#,";
+            return res;
+        }
+        res = res + root.val + ",";
+        res = res + Serialize(root.left);
+        res = res + Serialize(root.right);
+        return res;
+    }
+
+
+    /**
+     * 判断是否为对称的二叉树，用中序遍历解决
+     * @param root
+     * @return
+     */
+    public boolean isSymmtric(TreeNode root) {
+        if (null == root) {
+            return true;
+        }
+        ArrayList<String> res = new ArrayList<>();
+        printTreeByMidium(root, res);
+        // 判断数据奇偶对称性
+        int size = res.size();
+        if (size % 2 == 0) {
+            return false;
+        }
+        int center = size / 2;
+        int i = center;
+        int j = center;
+        while (i >= 0 && j < size) {
+            if (!res.get(i).equals(res.get(j))) {
+                return false;
+            }
+            i--;
+            j++;
+        }
+        return true;
+    }
+
+    /**
+     * 比较两颗树是否对称
+     * @param root1
+     * @param root2
+     * @return
+     */
+    public boolean compareTwoTreeNode(TreeNode root1, TreeNode root2) {
+        if (null == root1 && null == root2) {
+            return true;
+        }
+        boolean res = false;
+        if (null != root1.left && null != root2.right && root1.left.val == root2.right.val) {
+            res = true;
+        }
+
+        if (null == root1.left && null == root2.right) {
+            res = true;
+        }
+
+        if (null == root1.right && null == root2.left) {
+            res = res && true;
+        }
+
+        if (null != root1.right && null != root2.left && root1.right.val == root2.left.val) {
+            res = res && true;
+        }
+
+        if (res) {
+            res = res && compareTwoTreeNode(root1.left, root2.right) && compareTwoTreeNode(root1.right, root2.left);
+        }
+        return res;
+    }
+
+    @Test
+    public void testMain() {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(5);
+
+        root.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(5);
+
+        //root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(5);
+        System.err.println(Serialize(root));
+
+        List<String> res = new ArrayList<>();
+        printTreeByMidium(root, res);
+        System.err.println(res);
+        System.err.println(isSymmtric(root));
     }
 }
