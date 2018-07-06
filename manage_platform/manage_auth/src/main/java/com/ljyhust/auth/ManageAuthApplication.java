fcd7be36a5da659587b3079b7e603b33138d3690
ljyhust <ljyhust@163.com>
2018-07-06 23:55:13 +0800

    [M]

---
+++ manage_platform/manage_auth/src/main/java/com/ljyhust/auth/ManageAuthApplication.java
@@ -0,0 +1,12 @@
+package com.ljyhust.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageAuthApplication.class, args);
+	}
+}
---
+++ manage_platform/manage_registry/src/main/java/com/ljyhust/registry/ManageRegistryApplication.java
@@ -0,0 +1,11 @@
+package com.ljyhust.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageRegistryApplication.class, args);
+    }
+}
